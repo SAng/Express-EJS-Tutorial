417d4b5be2bf0cba1f3be8e1ab13c105b25e93a7
Spencer Ang <boomerandzapper@gmail.com>
2017-05-08 09:06:39 +0000

    Completed tutorial but have not tested yet

---
+++ app.js
@@ -0,0 +1,60 @@
+/* app.js */
+
+// require and instantiate express
+const app = require('express')()
+
+// fake posts to simulate a database
+const posts = [
+  {
+    id: 1,
+    author: 'Cow',
+    title: 'Mooooo...',
+    body: "zzzzz..."
+  },
+  {
+    id: 2,
+    author: 'Snake',
+    title: 'ssssss....',
+    body: 'ssss...sssss....s'
+  },
+  {
+    id: 3,
+    author: 'Dog',
+    title: 'Woof!',
+    body: 'Grrrr...'
+  },
+  {
+    id: 4,
+    author: 'Cat',
+    title: 'Meow!',
+    body: 'Purrr...'
+  }
+]
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+// blog home page
+app.get('/', (req, res) => {
+  // render `home.ejs` with the list of posts
+  res.render('home1', { posts: posts })
+})
+
+// blog post
+app.get('/post/:id', (req, res) => {
+  // find the post in the `posts` array
+  const post = posts.filter((post) => {
+    return post.id == req.params.id
+  })[0]
+
+  // render the `post.ejs` template with the post content
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  })
+})
+
+app.listen(8080)
+
+console.log('listening on port 8080')
